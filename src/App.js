56613975417718e4a71f36c72f067fc32d14024e
Oluwatobiloba Oladunjoye <dunjoyebiz2020@gmail.com>
2024-02-15 17:02:28 +0100

    search function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Card from './components/card-list/card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users },
+        () => {
+          console.log(this.state);
+        }
+      ));
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState({ searchField });
+  };
+
+  render() {
+    const {searchField, monsters} = this.state
+    const {onSearchChange} = this;
+
+    const filteredMonster = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1>Monster List</h1>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search Monsters'
+          onChange={onSearchChange}
+        />
+      
+        {filteredMonster.map((monster, index) => {
+          const id = `monster_${index}`;
+          return <h1 key={id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card.js
@@ -0,0 +1,17 @@
+/* import { Component }  from 'react';
+
+class Card extends Component {
+    render() {
+        const { monsters } = this.props;
+
+        return (
+        <div>
+            {monsters.map((monster) => (
+                <h1>{monster.name}</h1>
+            ))}
+        </div>
+        );
+    }
+};
+
+export default Card; */
